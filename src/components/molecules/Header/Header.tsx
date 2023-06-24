ff83a2437fcefdc81d15986aaeb34918ec2ef9d4
Novi Dwi Cahya <novidwicahya19@gmail.com>
2023-06-24 23:04:36 +0700

    feat: add button logout on header section page users

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { Button } from '../../atoms';
 import './header.style.scss';
 const Header = () => {
+  const router = useRouter();
+
+  const actionLogout = () => router.push('/');
+
   return (
     <div className="_header_wrapper">
       <h1 className="_text_header">List Users</h1>
+      <Button onClick={actionLogout} variant="secondary">
+        Logout
+      </Button>
     </div>
   );
 };
