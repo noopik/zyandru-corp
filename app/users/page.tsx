712810aaa0e4d0f71312f3bd6c2825604f04e432
Novi Dwi Cahya <novidwicahya19@gmail.com>
2023-06-25 01:58:28 +0700

    perf: get data users on user list page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,7 +3,7 @@ import { ListUsers } from '@vhiweb/src/components/organism';
 import axiosConfig from '@vhiweb/src/config/axios';
 
 const Users = async () => {
-  const { data: users } = await getDataUsers();
+  const users = await getDataUsers();
 
   return (
     <main className="container mx-auto p-6">
@@ -14,14 +14,23 @@ const Users = async () => {
 };
 
 async function getDataUsers() {
-  const res = await axiosConfig.get('/users');
+  try {
+    const resPage1 = await axiosConfig.get('/users');
+    const resPage2 = await axiosConfig.get('/users?page=2');
 
-  if (!res.status) {
-    // This will activate the closest `error.js` Error Boundary
+    const isResponseSuccess = resPage1.status && resPage2.status;
+
+    if (!isResponseSuccess) {
+      // This will activate the closest `error.js` Error Boundary
+      throw new Error('Failed to fetch data');
+    }
+
+    const data = [...resPage1.data.data, ...resPage2.data.data];
+
+    return data;
+  } catch (error) {
     throw new Error('Failed to fetch data');
   }
-
-  return res.data;
 }
 
 export default Users;
